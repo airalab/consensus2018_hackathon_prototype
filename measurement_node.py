dacab6d67913c527eccc868f6a969c9ab9b5ca37
Alexander Krupenkin <mail@akru.me>
2018-05-12 17:01:10 +0300

    Added multicity query support

--- measurement_node.py
+++ measurement_node.py
@@ -1,18 +1,30 @@
 from model import session, Measurement, City
 from std_msgs.msg import String
+from std_srvs.srv import Empty
 import rospy
 
 if __name__ == '__main__':
     rospy.init_node('measurements_query', anonymous=True)
     pub = rospy.Publisher('measurement', String, queue_size=200)
+    finish = rospy.ServiceProxy('liability/finish', Empty)
 
     def select_city(msg):
         s = session()
-        city = s.query(City).filter(City.name == msg.data).one()
-        for m in s.query(Measurement).filter(Measurement.city_id == city.id):
-            res = String()
-            res.data = '{0}: {1} {2}'.format(m.stamp, m.value, m.unit)
-            pub.publish(res)
+        for city_name in msg.data.split(", "):
+            try:
+                city = s.query(City).filter(City.name == city_name).one()
+                for m in s.query(Measurement).filter(Measurement.city_id == city.id):
+                    res = String()
+                    res.data = '{0} => {1} : {2} {3}'.format(city_name, m.stamp, m.value, m.unit)
+                    pub.publish(res)
+            except:
+                pass
+        rospy.wait_for_service('liability/finish')
+        try:
+            finish()
+        except rospy.ServiceException as e:
+            print("Service call failed: {}".format(e))
+
     rospy.Subscriber('select_city', String, select_city)
 
     rospy.spin()
